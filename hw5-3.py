b2ee8cf88c1ba2ce89dba6f79df150f7148a39fb
p22cobrien <p22cobrien@fairfieldprep.org>
2021-10-29 11:03:27 -0400

    submit hw5-3.py

---
+++ hw5-3.py
@@ -0,0 +1,17 @@
+# Author: CMOB 10/29/2021
+import calendar
+
+# Question 1
+calendar.TextCalendar().pryear(2020)
+
+# Question 2
+calendar.setfirstweekday(6)
+
+# Question 3
+print(calendar.month(2020, 11))
+
+# Question 4
+print(calendar.LocaleTextCalendar(6, "SPANISH").pryear(2020))
+
+# Question 5
+print(calendar.isleap(2020))
